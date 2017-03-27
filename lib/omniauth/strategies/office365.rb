9260c66cb125cb0685ee4c65c8be2fe917f892a5
Jeff Carbonella <jeff@contactually.com>
2017-03-27 17:10:25 -0400

    Revert "Parse info from JWT where possible"
    This reverts commit 56d257358c37b652f84dff91b64b8881a0474100.
    
    In some cases, the `upn` in the JWT is actually different than
    the `EmailAddress` in the `/api/v2.0/me` response so we can't
    rely on the JWT for all the user's info.

--- lib/omniauth/strategies/office365.rb
+++ lib/omniauth/strategies/office365.rb
@@ -18,58 +18,25 @@ module OmniAuth
       # additional calls (if the user id is returned with the token
       # or as a URI parameter). This may not be possible with all
       # providers.
-      uid { parsed_uid }
+      uid { raw_info['Id'] }
 
       # https://github.com/intridea/omniauth/wiki/Auth-Hash-Schema#schema-10-and-later
       info do
         {
-          :name => jwt_info['name'] || raw_info['DisplayName'],
-          :email => parsed_email,
-          :first_name => jwt_info['given_name'],
-          :last_name => jwt_info['family_name']
+          :name => raw_info['DisplayName'],
+          :email => raw_info['EmailAddress'],
+          :nickname => raw_info['Alias']
         }
       end
 
       extra do
         {
-          :jwt_info => jwt_info,
           :raw_info => raw_info
         }
       end
 
       def raw_info
         @raw_info ||= access_token.get('api/v2.0/me').parsed
-      end
-
-      private
-
-      OFFICE365_JWT_USER_IDENTIFER = 'oid'.freeze
-      OFFICE365_JWT_ORGANIZATION_IDENTIFER = 'tid'.freeze
-      OFFICE365_JWT_USER_PRINCIPAL_NAME = 'upn'.freeze
-
-      def decoded_jwt_token
-        JWT.decode(access_token.token, nil, false)
-      rescue JWT::DecodeError
-        []
-      end
-
-      def jwt_info
-        @jwt_info ||= decoded_jwt_token.first || {}
-      end
-
-      def parsed_email
-        return raw_info['EmailAddress'] if jwt_info.empty?
-
-        jwt_info[OFFICE365_JWT_USER_PRINCIPAL_NAME]
-      end
-
-      def parsed_uid
-        return raw_info['Id'] if jwt_info.empty?
-
-        [
-          jwt_info[OFFICE365_JWT_USER_IDENTIFER],
-          jwt_info[OFFICE365_JWT_ORGANIZATION_IDENTIFER]
-        ].join('@')
       end
     end
   end
